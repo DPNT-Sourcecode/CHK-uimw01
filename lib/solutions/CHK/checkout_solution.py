0bce69f6f94db4d664d6e95f3d5ef9ff1c1d28a0
sbx_user1059 <sbx_user1059@ip-10-129-242-182.eu-west-2.compute.internal>
2018-09-26 09:48:18 +0000

    Wed Sep 26 09:48:18 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,21 @@ PRICE = {"A":50,
         "C":20,
         "D":15}
 
+
+def _is_sku_illegal(skus):
+    for item in skus:
+        if item not in PRICE:
+            return True
+    return False
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     total_checkout = 0
+
+    if _is_sku_illegal(skus):
+        return -1
 
 
 
